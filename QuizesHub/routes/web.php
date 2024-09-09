d99d731d04214ff9384393cb0c5effed42621716
Ahmed Gad <116387176+ahmed-atef-gad@users.noreply.github.com>
2024-09-09 17:41:37 +0300

    add dashboard

--- QuizesHub/routes/web.php
+++ QuizesHub/routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExamAttemptController;
+use App\Http\Controllers\HomeController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/home', HomeController::class)->name('home');
+});
