f243e7e13081cf464892ba27f33ac198c598c675
Ahmed Gad <116387176+ahmed-atef-gad@users.noreply.github.com>
2024-09-09 20:14:30 +0300

    add assets

--- QuizesHub/resources/views/dashboard/layout/head.blade.php
+++ QuizesHub/resources/views/dashboard/layout/head.blade.php
@@ -16,3 +16,5 @@
     <link rel="stylesheet" href="{{ asset('dashboard/assets') }}/css/style.css">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    @yield('styles')
