4af59f86a21298b602ace89ef6489c85f81552ff
mohamednayef <mohamednayef2002@gmail.com>
2024-09-12 15:29:36 +0300

    add scripts and styels on exams

--- QuizesHub/resources/views/dashboard/exams/index.blade.php
+++ QuizesHub/resources/views/dashboard/exams/index.blade.php
@@ -1,6 +1,16 @@
 {{-- @dd($exams) --}}
 @extends('dashboard.layout.master')
 
+
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('dashboard/assets') }}/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet"
+        href="{{ asset('dashboard/assets') }}/vendors/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet"
+        href="{{ asset('dashboard/assets') }}/vendors/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css">
+@endsection
+
+
 @section('content')
   <div class="content mt-3">
     <div class="animated fadeIn">
@@ -56,4 +66,26 @@
         </div>
     </div><!-- .animated -->
   </div><!-- .content -->
+@endsection
+
+@section('scripts')
+    <!-- Make sure jQuery is loaded first -->
+    <script src="{{ asset('dashboard/assets/vendors/jquery/jquery.min.js') }}"></script>
+
+    <!-- DataTables JS -->
+    <script src="{{ asset('dashboard/assets/vendors/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/vendors/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+
+    <!-- Additional JS for DataTables functionality -->
+    <script src="{{ asset('dashboard/assets/vendors/jszip/dist/jszip.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/vendors/pdfmake/build/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/vendors/pdfmake/build/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/vendors/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/vendors/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('dashboard/assets/vendors/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
+
+    <!-- DataTables Initialization Script -->
+    <script src="{{ asset('dashboard/assets/js/init-scripts/data-table/datatables-init.js') }}"></script>
 @endsection
