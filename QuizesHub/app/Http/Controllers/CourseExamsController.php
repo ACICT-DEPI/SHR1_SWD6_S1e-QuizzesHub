28f6d6caa90ab4214b295bd1cef5efa380d89504
ahmedebrahimabdrabo <ahmedebrahim011147349160@gmail.com>
2024-10-01 23:43:10 +0300

    CourseExams Show

---
+++ QuizesHub/app/Http/Controllers/CourseExamsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Admin\Course;
+
+class CourseExamsController extends Controller
+{
+    public function CourseExams($course)
+    {
+        $course = Course::find($course);
+        $exams = $course->exams;
+         return view('site.pages.CourseExams',compact('exams'));
+    }
+}
---
+++ QuizesHub/resources/views/site/pages/CourseExams.blade.php
@@ -0,0 +1,14 @@
+@extends('site.layout.master')
+@section('content')
+
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title bg-white text-center text-primary px-3">Exams</h6>
+            <h1 class="mb-5">Course Exams</h1>
+        </div>
+        </div>
+        <div allign="center"></div>
+                  
+        
+@endsection
