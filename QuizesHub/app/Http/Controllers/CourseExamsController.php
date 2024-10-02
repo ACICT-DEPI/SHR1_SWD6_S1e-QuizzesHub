dfafe92d207dc48f4ea4de36ab67d15254490218
Ahmed Gad <116387176+ahmed-atef-gad@users.noreply.github.com>
2024-10-02 19:29:29 +0300

    show course exams

--- QuizesHub/app/Http/Controllers/CourseExamsController.php
+++ QuizesHub/app/Http/Controllers/CourseExamsController.php
@@ -12,15 +12,15 @@ use Illuminate\Support\Facades\Auth;
 class CourseExamsController extends Controller
 {
     public function CourseExams($course)
-    {  
+    {
         $course_id = CourseFacultyMajorUniversity::where('course_id',$course)
         ->where('university_id',Auth::user()->university_id)
         ->where('faculty_id',Auth::user()->faculty_id)
         ->where('major_id',Auth::user()->major_id)
-        ->first()->course_id;
-        $course=Course::where('id',$course_id)->first();
-        $exams = $course->exams;
-      
+        ->first();
+        $course=Course::where('id',$course)->first();
+        $exams = $course_id->exams;
+
          return view('site.pages.CourseExams',compact('exams','course'));
     }
 }
