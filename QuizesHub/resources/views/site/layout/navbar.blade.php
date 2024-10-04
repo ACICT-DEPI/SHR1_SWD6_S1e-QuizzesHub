0ca3766e74aae444ca5753565c9657cfb6d79f08
Ahmed Gad <116387176+ahmed-atef-gad@users.noreply.github.com>
2024-10-04 20:46:24 +0300

    edit width

--- QuizesHub/resources/views/site/layout/navbar.blade.php
+++ QuizesHub/resources/views/site/layout/navbar.blade.php
@@ -12,30 +12,30 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
             @auth
-            @if(Auth::user()->email_verified_at == null)
-            <a href="{{ route('verification.notice') }}" class="nav-item nav-link active">Verify Email</a>
-            <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Settings</a>
-                <div class="dropdown-menu fade-down m-0">
+                @if(Auth::user()->email_verified_at == null)
+                    <a href="{{ route('verification.notice') }}" class="nav-item nav-link active">Verify Email</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Settings</a>
+                        <div style="min-width: 0px !important; width:110px;" class="dropdown-menu fade-down m-0">
 
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                            this.closest('form').submit();"> Logout</a>
+                            <form  method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a  class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                    this.closest('form').submit();">Logout</a>
 
-                    </form>
-                </div>
-            </div>
-            @else
+                            </form>
+                        </div>
+                    </div>
+                @else
 
 
-                <a href="{{ route('site.index') }}" class="nav-item nav-link active">Home</a>
-                <a href="about.html" class="nav-item nav-link">About</a>
+                    <a href="{{ route('site.index') }}" class="nav-item nav-link active">Home</a>
+                    <a href="about.html" class="nav-item nav-link">About</a>
 
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Courses</a>
-                    <div class="dropdown-menu fade-down m-0">
-                        @if(!empty(Auth::user()->courses()))
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Courses</a>
+                        <div class="dropdown-menu fade-down m-0">
+                            @if(!empty(Auth::user()->courses()))
                             @php
                             $courses = [];
                             foreach(Auth::user()->courses() as $course){
@@ -47,31 +47,32 @@
                             <a href="{{ route('CourseExams',$course->id) }}" class="dropdown-item">{{ $course->name }}</a>
                             @endforeach
 
-                        @else
+                            @else
                             <span class="dropdown-item">No Course</span>
-                        @endif
+                            @endif
 
+                        </div>
                     </div>
-                </div>
-                <a href="contact.html" class="nav-item nav-link">Contact</a>
+                    <a href="contact.html" class="nav-item nav-link">Contact</a>
 
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Settings</a>
-                    <div class="dropdown-menu fade-down m-0">
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Settings</a>
+                        <div style="min-width: 0px !important; width:110px;" class="dropdown-menu fade-down m-0">
 
-                        <a href="{{ route('profile.edit') }}" class="dropdown-item">profile</a>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                this.closest('form').submit();"> Logout</a>
+                            <a href="{{ route('profile.edit') }}" class="dropdown-item">profile</a>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                        this.closest('form').submit();"> Logout</a>
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
-                </div>
 
-            </div>
+                </div>
             @endif
         @endauth
+
         @guest
         <a href="{{ route('register') }}" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i
                 class="fa fa-arrow-right ms-3"></i></a>
@@ -79,8 +80,10 @@
 
     </div>
 </nav>
+@section('styles')
 <style>
     body {
         overflow-x: hidden;
     }
 </style>
+@endsection
