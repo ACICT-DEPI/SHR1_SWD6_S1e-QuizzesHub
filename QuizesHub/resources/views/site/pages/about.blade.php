143588fc44cda32fea3a077a079622ac4d3b7cc9
ahmedebrahimabdrabo <ahmedebrahim011147349160@gmail.com>
2024-10-06 00:33:22 +0300

    Ana Teabt

--- QuizesHub/resources/views/site/pages/about.blade.php
+++ QuizesHub/resources/views/site/pages/about.blade.php
@@ -15,22 +15,22 @@
                 <p class="mb-4"> It offers a wide range of quizzes covering various topics, from general knowledge and current events to niche subjects like science, history, pop culture, and more. Whether you're looking to challenge yourself, learn something new, or just have fun, Quizzes Hub provides an engaging and interactive way to test your knowledge. With a user-friendly interface and regularly updated content, it’s a go-to destination for anyone looking to sharpen their wits and enjoy some friendly competition.</p>
                 <div class="row gy-2 gx-4 mb-4">
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>More Courses</p>
                     </div>
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Exams</p>
                     </div>
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>FeadBacks About Exams</p>
                     </div>
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Comments </p>
                     </div>
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Certification</p>
                     </div>
                     <div class="col-sm-6">
-                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
+                        <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Fast Response</p>
                     </div>
                 </div>
                 <a class="btn btn-primary py-3 px-5 mt-2" href="{{route('site.ReadMore')}}">Read More</a>
