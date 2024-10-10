80c665795a83c93f8e620a1c5f2098f625ff7368
mohamednayef <mohamednayef2002@gmail.com>
2024-10-10 04:49:24 +0300

    update web.php

--- QuizesHub/resources/views/site/layout/navbar.blade.php
+++ QuizesHub/resources/views/site/layout/navbar.blade.php
@@ -12,7 +12,7 @@
         @else 
             <img src="{{ asset('dashboard/assets/images/default.jpg') }} " alt="" width="30" height="30" class="rounded-circle">
         @endif 
-        {{ Auth::user()->username }} | {{ Auth::user()->score }}
+        {{ Auth::user()->fname }} | {{ Auth::user()->score }}
     </a>
     @endauth
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
